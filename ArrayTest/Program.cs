f6ad7ca7e71ef6f8de838a7df8569a163cc76c79
mvdoyle <mdoyle@truecoders.io>
2020-04-08 13:40:11 -0500

    added question and scoring

--- ArrayTest/Program.cs
+++ ArrayTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ArrayTest
 {
@@ -6,6 +7,8 @@ namespace ArrayTest
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = Stopwatch.StartNew();
+
             Console.WriteLine("Hello!  This will be an exerecise in array and list creation!");
             Console.WriteLine();
             var answer1 = "var numbers = new int[5];";
@@ -15,8 +18,10 @@ namespace ArrayTest
             var answer5 = "teachers.Add(\"Michael\");";
             var answer6 = "teachers.Remove(\"Michael\");";
             var answer7 = "numbers[0] = 10;";
+            var answer8 = "Count";
 
             var guess = "";
+            var wrongCount = 0;
 
             do
             {
@@ -27,9 +32,14 @@ namespace ArrayTest
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
                 }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
 
             } while (answer1 != guess);
-
+            
             do
             {
                 Console.WriteLine("Using explicit typing: create a new array named numbers that will hold 5 integers:");
@@ -39,17 +49,27 @@ namespace ArrayTest
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
                 }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
 
             } while (answer2 != guess);
 
             do
             {
-                Console.WriteLine("Add the value, 10, to the numbers array at index 0:");
+                Console.WriteLine("Place the value, 10, in the numbers array at index 0:");
                 guess = Console.ReadLine();
                 if (guess == answer7)
                 {
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
                 }
 
             } while (answer7 != guess);
@@ -64,6 +84,11 @@ namespace ArrayTest
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
                 }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
 
             } while (answer3 != guess);
 
@@ -75,6 +100,11 @@ namespace ArrayTest
                 {
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
                 }
 
             } while (answer4 != guess);
@@ -88,6 +118,11 @@ namespace ArrayTest
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
                 }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
 
             } while (answer5 != guess);
 
@@ -100,8 +135,43 @@ namespace ArrayTest
                     Console.WriteLine("Correct!");
                     Console.WriteLine();
                 }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
 
             } while (answer6 != guess);
+
+            do
+            {
+                Console.WriteLine("The following code will store the size of the teachers list in a variable");
+                Console.WriteLine("but the code is missing to complete this");
+                Console.WriteLine("What property will complete the code?");
+                Console.WriteLine("var size = teachers._______;");
+                guess = Console.ReadLine();
+                if (guess == answer8)
+                {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                    wrongCount++;
+                }
+
+            } while (answer8 != guess);
+
+            var time = sw.Elapsed.Seconds;
+
+            Console.WriteLine("Total seconds to complete:");
+            Console.WriteLine(time);
+
+            var score = (time * .7) + (wrongCount * .3);
+
+            Console.WriteLine("Your Score:");
+            Console.WriteLine(score);
         }
     }
 }
